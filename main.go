13a1b2c5322a87675d1eede8f0bb487b24b9904e
Ahmad Berahman <62759025+a-berahman@users.noreply.github.com>
2021-04-25 15:16:33 +0430

    original add service and translator

--- main.go
+++ main.go
@@ -6,15 +6,12 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/a-berahman/pc-offline-challenge/common"
-	"github.com/a-berahman/pc-offline-challenge/services"
 	"golang.org/x/text/language"
 )
 
 func main() {
 	ctx := context.Background()
 	rand.Seed(time.Now().UTC().UnixNano())
-	translerSerivce := services.GetService(ctx, common.TranlatorService).(services.Translator)
-
-	fmt.Println(translerSerivce.Translate(language.English, language.Japanese, "test"))
+	s := NewService()
+	fmt.Println(s.translator.Translate(ctx, language.English, language.Japanese, "test"))
 }
--- services/translator.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import "golang.org/x/text/language"
-
-//Translator is implemented by objects that promote Cache repository
-type Translator interface {
-	//Translate returns translate value as a string
-	Translate(from, to language.Tag, data string) (string, error)
-}
